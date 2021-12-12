4e5edbe4c74b653cf2463abe18ddb28890289d2d
TINHOFER Jakob <jak.tinhofer@billrothgymnasium.at>
2021-12-12 21:56:36 +0100

    Revamped main site

--- app/svelte/src/pageTemplates/Main.svelte
+++ app/svelte/src/pageTemplates/Main.svelte
@@ -7,7 +7,7 @@
 
     let loggedIn;
     
-
+    let sections = [];
     
 
     updateLoggedIn();
@@ -38,6 +38,11 @@
         console.log("Clicked.");
         logOut().then((res) => {updateLoggedIn()}, ex => {console.debug(ex);})
     }
+
+    function nav(elem){
+        
+    }
+
 </script>
 
 <style>
@@ -45,62 +50,137 @@
         margin: 0px;
     }
     .header{
+        height: 100vh;
         background-color: royalblue;
         color: white;
         text-align: center;
-        padding:  0px 0px 20vw 0px ;
+        position: relative;
     }
     .header h1{
-        margin-top: 10vw;
-        font-size: 6vw;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        font-size: 70px;
+        transform: translate(-50%, -50%);
     }
     .fa-user{
         margin-right: 10px;
     }
 
     #generelle-infos{
-        padding: 50px;
+        width: 60%;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
     }
 
     #generelle-infos h1{
         font-family: 'Roboto', sans-serif;
+        font-size: 30px;
+        color: white;
+        background-color: black;
+        display: inline-block;
+        padding: 5px;
+        margin-bottom: 10px;
+    }
+
+    #generelle-infos p{
+        font-size: 25px;
+    }
+
+    section{
+        height: 100vh;
+        scroll-snap-align: center;
+        position: relative;
+    }
+    .nav{
+        position: absolute;
+        left: 50%;
+        font-size: 40px;
+        transition: 0.6s opacity;
+        opacity: 30%;
+    }
+
+    .nav:hover{
+        opacity: 100%;
+    }
+
+    .next{
+        bottom: 0%;
+        transform: translate(-50%, -105%);
+    }
+
+    .prev{
+        top: 0%;
+        transform: translate(-50%, +105%);
+    }
+
+    .parent{
+        overflow: auto;
+        width: 100vw;
+        height: 100vh;
+        scroll-snap-type: y mandatory;
+        padding-right: 20px;
+        
+    }
+    .parent section{
+        width: 100vw;
     }
 </style>
 
 <svelte:head>
     <title>WINF Matura 2021/2022</title>
+    <style>
+        body{
+            overflow: hidden;
+        }
+    </style>
 </svelte:head>
 
-
-<div class="header">
-    <Navbar>
-        <svelte:fragment>
-            <NavbarElement>Generelle Infos</NavbarElement>
-            <NavbarElement>Themen</NavbarElement>
-            <NavbarElement>Mehr</NavbarElement>
-            <NavbarElement on:click="{() => {window.location = "pages"}}">Pages</NavbarElement>
-            {#if loggedIn}
-                <span style="float:right">
-                    <Dropdown>
-                        <NavbarElement>
-                            <i class="fas fa-user"></i>
-                            <p>{user !== undefined ? "Hallo " + user.username : "Account"}</p>
-                        </NavbarElement>
-                        <svelte:fragment slot="dropdown-list-elems">
-                            <NavbarElement on:click="{logOutClicked}">Log Out</NavbarElement>
-    
-                            <NavbarElement>Account info</NavbarElement>
-                        </svelte:fragment>
-                    </Dropdown>
-                </span>   
-            {:else}
-                <NavbarElement on:click={handleAccountButtonClicked} float="right"><i class="fas fa-user"></i><p>Log In</p></NavbarElement>
-            {/if}
-            
-        </svelte:fragment>
-    </Navbar>
-    <h1>Hello Traveler</h1>
-</div>
-<div id="generelle-infos">
-    <h1>Generelle Infos zur Informatikmatura 2021/2022</h1>
+<div class="parent">
+    <section class="header">
+        <Navbar>
+            <svelte:fragment>
+                <NavbarElement>About the Matura</NavbarElement>
+                <NavbarElement>Topics</NavbarElement>
+                <NavbarElement>More</NavbarElement>
+                <NavbarElement on:click="{() => {window.location = "pages"}}">Pages</NavbarElement>
+                {#if loggedIn}
+                    <span style="float:right">
+                        <Dropdown>
+                            <NavbarElement>
+                                <i class="fas fa-user"></i>
+                                <p>{user !== undefined ? "Hallo " + user.username : "Account"}</p>
+                            </NavbarElement>
+                            <svelte:fragment slot="dropdown-list-elems">
+                                <NavbarElement on:click="{logOutClicked}">Log Out</NavbarElement>
+        
+                                <NavbarElement>Account info</NavbarElement>
+                            </svelte:fragment>
+                        </Dropdown>
+                    </span>   
+                {:else}
+                    <NavbarElement on:click={handleAccountButtonClicked} float="right"><i class="fas fa-user"></i><p>Log In</p></NavbarElement>
+                {/if}
+                
+            </svelte:fragment>
+        </Navbar>
+        <h1>Matura in Informatics 2021/2022</h1>
+        <i class="fas fa-chevron-down nav next" on:click={() => {nav(-1);}}></i>
+    </section>
+    <section>
+        <i class="fas fa-chevron-up nav prev" style="color: black" on:click={() => {nav(1)}}></i>
+        <div id="generelle-infos">
+            <h1>A brief Overview of the Matura in Informatics 2021/2022</h1>
+            <p>The Matura, also called Zentralmatura or SRDP, allows absolvents to attend universities and colleges.
+             Normally taken by students after high school, the exam tests knowledge in 6 different subjects.
+             While German, Mathematics and a living second language are required, the other 3 subjects can be chosen by the student.
+             All questions from the exam are always from a few distinct topics.
+             If you wanted to choose Informatics as one of your subjects, you might want to look at some preparation material first.
+             This is what we hope to provide here.
+            </p>
+        </div>
+        <i class="fas fa-chevron-down nav next" style="color: black" on:click="{() => {nav(-1)}}"></i>
+    </section>
 </div>
